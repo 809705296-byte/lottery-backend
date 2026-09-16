06dbea173057da116f8a069af6a5915f6fcfe039
809705296-byte <809705296@qq.com>
2026-09-16 14:57:20 +0800

    Delete lottery-backend directory

--- lottery-backend/edge-functions/hello.js
+++
@@ -1,5 +0,0 @@
-export default async function onRequest(context) {
-  return new Response('hello world', {
-    headers: { 'Content-Type': 'text/plain; charset=UTF-8' }
-  });
-}
--- lottery-backend/edge-functions/index.js
+++
@@ -1,5 +0,0 @@
-export default async function onRequest() {
-  return new Response('OK from edge-functions', {
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
